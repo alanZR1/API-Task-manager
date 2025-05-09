7601ec4cff7e5b5cec6f3c82b795d975d74eb9f6
Alan Reyes Luis <alannreyes02@gmail.com>
2025-05-08 19:03:34 -0600

    agregando la app a installed app

--- gestor_tareas/gestor_tareas/settings.py
+++ gestor_tareas/gestor_tareas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tareas'
+    'tareas',
+    'rest_framework',  # Agregar django-rest-framework
 ]
 
 MIDDLEWARE = [
