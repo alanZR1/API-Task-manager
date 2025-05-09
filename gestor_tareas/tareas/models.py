07676631a00f1a9f4b37c8e2ae040812c2fa324f
Alan Reyes Luis <alannreyes02@gmail.com>
2025-05-08 18:17:03 -0600

    agregando el modelo Tarea

--- gestor_tareas/tareas/models.py
+++ gestor_tareas/tareas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self):
+        return self.nombre
